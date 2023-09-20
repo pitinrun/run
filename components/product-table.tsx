ba50dab5fdd8fc1b79742619fd24944afb7dce6b
Kiju2 <thdydehfdl12@gmail.com>
2023-09-21 02:43:38 +0900

    feat: Modify product table

--- components/product-table.tsx
+++ components/product-table.tsx
@@ -23,47 +23,47 @@ export default function ProductTable({ products }: { products: IProduct[] }) {
         {/* head */}
         <thead>
           <tr>
-            <th>브랜드</th>
+            {/* <th>브랜드</th> */}
             <th>제품명(영문)</th>
-            <th>제품명(한글)</th>
+            {/* <th>제품명(한글)</th> */}
             <th>사이즈</th>
             <th>SS/LR</th>
             <th>마킹</th>
             <th>원산지</th>
-            <th>계절</th>
-            <th>특수</th>
-            <th>기타</th>
+            {/* <th>계절</th> */}
+            {/* <th>특수</th> */}
+            {/* <th>기타</th> */}
             <th>공장도가격</th>
             <th>할인율</th>
-            <th>총 수량</th>
-            {storageNames.map((storageName, index) => (
+            {/* <th>총 수량</th> */}
+            {/* {storageNames.map((storageName, index) => (
               <th key={index}>{storageName}</th>
-            ))}
+            ))} */}
           </tr>
         </thead>
         <tbody>
           {products.map((product, index) => (
             <tr key={index}>
-              <td>{product.brand}</td>
+              {/* <td>{product.brand}</td> */}
               <td>{product.pattern}</td>
-              <td>{product.patternKr}</td>
+              {/* <td>{product.patternKr}</td> */}
               <td>{product.size}</td>
               <td>{product.speedSymbolLoadIndex}</td>
               <td>{product.marking}</td>
               <td>{product.origin}</td>
-              <td>{product.season}</td>
-              <td>{product.special}</td>
-              <td>{product.etc}</td>
+              {/* <td>{product.season}</td> */}
+              {/* <td>{product.special}</td> */}
+              {/* <td>{product.etc}</td> */}
               <td>{product.factoryPrice}</td>
               <td>{product.specialDiscountRate}</td>
-              <td>
+              {/* <td>
                 {product.storages.reduce((acc, currentValue, currentIndex) => {
                   return acc + currentValue.stock;
                 }, 0)}
               </td>
               {product.storages.map((storage, index) => (
                 <td key={index}>{storage.stock}</td>
-              ))}
+              ))} */}
             </tr>
           ))}
         </tbody>
